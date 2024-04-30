71b256971ceb679e90f5bbdabe327b35e3634c50
it22242204 <128572527+it22242204@users.noreply.github.com>
2024-05-01 00:56:38 +0530

    error fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,23 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap-icons/font/bootstrap-icons.css";  
-import { Provider } from 'react-redux';
-import { store } from './store';
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Provider store={store}>
-      <App />
-    </Provider>
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
-
-
