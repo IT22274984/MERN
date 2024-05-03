9ffbc258c7f6e3ed172adfee59e0e4886fb1cd18
Ashen <thavaratnamashendra@gmail.com>
2024-05-03 15:58:38 +0530

    updated backend

--- backend/Server.js
+++ backend/Server.js
@@ -1,12 +1,27 @@
+// app.js
+
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors")
-const connectDB = require("./db");
+const cors = require("cors");
+const mongoose = require('mongoose');
 
 const app = express();
 
 // Load environment variables from .env file
 dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
 // Connect to MongoDB
 connectDB();
@@ -18,7 +33,6 @@ app.use(express.json());
 
 // Set up your routes
 app.use('/', require('./routes/appoinmentRoutes'));
-
 
 const PORT = process.env.PORT || 8080;
 
