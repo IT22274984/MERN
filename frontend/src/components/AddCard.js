33094d54a26f68ed1f296f455ff1032584266195
IT22125484 <it22125484@my.sliit.lk>
2024-05-09 10:32:18 +0530

    done

--- frontend/src/components/AddCard.js
+++ frontend/src/components/AddCard.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { addCardToServer } from "../slices/tasksSlice";
-import { useDispatch } from 'react-redux';
-import "./AddTask.css";
+import { addCardToServer } from "../slices/cardSlices";
+import Container from "react-bootstrap/Container";
+import { Row, Col } from "react-bootstrap";
+import "./AddTask.css"; // Import your custom CSS file
+import card from './card.png';
 
 const AddCardForm = () => {
     const dispatch = useDispatch();
     
+    const { error } = useSelector((state) => state.cards);
+
     const [cardNum, setCardNum] = useState('');
     const [holderName, setHolderName] = useState('');
     const [cardType, setCardType] = useState('');
@@ -22,7 +27,6 @@ const AddCardForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Reset errors
         setErrors({
             cardNum: '',
             cvv: '',
@@ -46,86 +50,97 @@ const AddCardForm = () => {
             return;
         }
 
-        console.log({ cardNum, holderName, cardType, exDate, cvv });
         dispatch(addCardToServer({ cardNum, holderName, cardType, exDate, cvv }));
 
-        // Clear form fields
         setCardNum('');
         setHolderName('');
         setCardType('');
         setExDate('');
         setCvv('');
-        alert("Successfully Created")
+        alert("Successfully Created");
     };
 
     return (
-        
-        <section className="my-5">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formCardNum">
-                    <Form.Label>Card Number</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter Card Number"
-                        value={cardNum}
-                        onChange={(e) => setCardNum(e.target.value)}
-                    />
-                    {errors.cardNum && <Form.Text className="text-danger">{errors.cardNum}</Form.Text>}
-                </Form.Group>
+        <>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col lg="6">
+                        <section className="my-5 card-form-container_pay">
+                            <div className="card-img-container_pay">
+                                <img src={card} alt="card" className="card-image_pay" />
+                                <Form onSubmit={handleSubmit} className="card-form_pay">
+                                    <Form.Group className="mb-3" controlId="formCardNum">
+                                        <Form.Label className="form-label_pay">Card Number</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter Card Number"
+                                            value={cardNum}
+                                            onChange={(e) => setCardNum(e.target.value)}
+                                        />
+                                        {errors.cardNum && <Form.Text className="error-message_pay">{errors.cardNum}</Form.Text>}
+                                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formHolderName">
-                    <Form.Label>Card Holder Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter Holder Name"
-                        value={holderName}
-                        onChange={(e) => setHolderName(e.target.value)}
-                    />
-                </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formHolderName">
+                                        <Form.Label className="form-label_pay">Card Holder Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter Holder Name"
+                                            value={holderName}
+                                            onChange={(e) => setHolderName(e.target.value)}
+                                        />
+                                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formCardType">
-                    <Form.Label>Card Type</Form.Label>
-                    <Form.Control
-                        as="select"
-                        value={cardType}
-                        onChange={(e) => setCardType(e.target.value)}
-                    >
-                        <option value=""> Select Card Type </option>
-                        <option value="visa">Visa</option>
-                        <option value="master">Master</option>
-                    </Form.Control>
-                </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formCardType">
+                                        <Form.Label className="form-label_pay">Card Type</Form.Label>
+                                        <Form.Control
+                                            as="select"
+                                            value={cardType}
+                                            onChange={(e) => setCardType(e.target.value)}
+                                        >
+                                            <option value=""> Select Card Type </option>
+                                            <option value="visa">Visa</option>
+                                            <option value="master">Master</option>
+                                        </Form.Control>
+                                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formExDate">
-                    <Form.Label>Expiration Date</Form.Label>
-                    <Form.Control
-                        type="date"
-                        placeholder="Enter Expiration Date"
-                        value={exDate}
-                        onChange={(e) => setExDate(e.target.value)}
-                    />
-                    {errors.exDate && <Form.Text className="text-danger">{errors.exDate}</Form.Text>}
-                </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formExDate">
+                                        <Form.Label className="form-label_pay">Expiration Date</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            placeholder="Enter Expiration Date"
+                                            value={exDate}
+                                            onChange={(e) => setExDate(e.target.value)}
+                                        />
+                                        {errors.exDate && <Form.Text className="error-message_pay">{errors.exDate}</Form.Text>}
+                                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formCvv">
-                    <Form.Label>CVV</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter CVV"
-                        value={cvv}
-                        onChange={(e) => setCvv(e.target.value)}
-                    />
-                    {errors.cvv && <Form.Text className="text-danger">{errors.cvv}</Form.Text>}
-                </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formCvv">
+                                        <Form.Label className="form-label_pay">CVV</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter CVV"
+                                            value={cvv}
+                                            onChange={(e) => setCvv(e.target.value)}
+                                        />
+                                        {errors.cvv && <Form.Text className="error-message_pay">{errors.cvv}</Form.Text>}
+                                    </Form.Group>
 
-                <div className="text-end">
-                    <Button variant="primary" type="submit">
-                        Add Card
-                    </Button>
-                </div>
-                
-            </Form>
-        </section>
+                                    <div className="text-end">
+                                        <Button variant="primary" type="submit">
+                                            Add Card
+                                        </Button>
+                                    </div>
+                                </Form>
+                            </div>
+                        </section>
+                        {error !== '' && <h5 className="text-center text-danger">{error}</h5>}
+                        <a href="/cards-list" className="premium-button_pay">Go to cards List</a>
+                    </Col>
+                </Row>
+
+            </Container>
+             
+        </>
     );
 };
 
